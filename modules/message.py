9a72dbf1009fa7ca3153736765a4355766b22158
Diogo Bernardelli <diogo@diogobernardelli.com.br>
2024-06-29 17:27:19 -0700

    .

--- modules/message.py
+++ modules/message.py
@@ -3,8 +3,6 @@ import json
 
 from dotenv import load_dotenv
 from modules.create_user import create_user_function
-# from modules.calendar.functions import list_event
-from modules.calendar.call_setup import get_calendar_service, generate_auth_url, create_event
 
 load_dotenv()
 
@@ -25,20 +23,16 @@ def message_function(request_data):
   openai_key = globals()["OPENAI_KEY"]
 
   user = create_user_function(request_data)
-  user_data = json.loads(user)
 
   print("============")
   print(user)
   print("============")
 
+  user_data = json.loads(user)
 
 
   # OPENAI CODE GOES HERE!!!
-  # message = get_calendar_service()
-  # message = generate_auth_url(user_data['phone_number'])
-  create_event(user_data['phone_number'])
 
 
-  # message = f"Hello, {user_data['name']}. How can I help you today?"
-  message = "Event created successfully!"
+  message = f"Hello, {user_data['name']}. How can I help you today?"
   return message
